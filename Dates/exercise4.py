a4dc875139400b5bd667991253021735cda33815
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-02-18 23:04:38 +0600

    Add files via upload

--- Dates/exercise4.py
+++ Dates/exercise4.py
@@ -1,9 +1,14 @@
 import datetime
 
 today_day=datetime.datetime.now()
-some_day=datetime.datetime(2024,12,8)
+some_day=datetime.datetime(2024,1,18)
+
 if today_day>some_day:
     difference=today_day-some_day
+
+elif today_day==some_day:
+    difference=0
+
 else:
     difference=some_day-today_day
 
